047ef50fda04fe285d728574a764eb08c4733a20
edward <son@binhsonnguyen.com>
2020-05-01 12:22:47 +0700

    include html file in dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/js/main.js',
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    })
+  ]
 }
